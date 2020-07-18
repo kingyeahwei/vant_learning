26ea1d0e52799f51cd4ecc589038498b5ebb58b2
jinyanwei <jinyanwei@live.com>
2020-07-18 23:53:46 +0800

    cell 垂直居中

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,7 @@
 <template>
   <div id="app">
     <div style="height: 10px"></div>
-    <van-cell value="内容" is-link>
-      <template #title>
-        <span class="custom-title">单元格</span>
-        <van-tag type="danger">标签</van-tag>
-      </template>
-    </van-cell>
-    <van-cell title="单元格" icon="shop-o">
-      <template #right-icon>
-        <van-icon name="search" style="font-size: 16px; line-height: inherit;"></van-icon>
-      </template>
-    </van-cell>
+    <van-cell center title="单元格" value="内容" label="描述信息"></van-cell>
   </div>
 </template>
 
