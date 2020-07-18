f3915776dab4424fe0bf3ad25b36e06a11d42aa7
jinyanwei <jinyanwei@live.com>
2020-07-19 00:16:00 +0800

    image 填充模式

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <van-image height="100" width="100" src="https://img.yzcdn.cn/vant/cat.jpeg"></van-image>
+    <van-image height="10rem" width="10rem" fit="contain" src="https://img.yzcdn.cn/vant/cat.jpeg"></van-image>
   </div>
 </template>
 
