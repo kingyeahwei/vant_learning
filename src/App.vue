4b635e403dec3400836721d62a2a5afe478c45d5
jinyanwei <jinyanwei@live.com>
2020-07-19 00:02:35 +0800

    icon 图标大小

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,8 @@
 <template>
   <div id="app">
     <div style="height: 10px"></div>
-    <van-icon name="chat-o" dot></van-icon>
-    <van-icon name="chat-o" badge="9"></van-icon>
-    <van-icon name="chat-o" badge="99+"></van-icon>
-    <van-icon name="chat-o" color="#1989fa"></van-icon>
-    <van-icon name="chat-o" color="#07c160"></van-icon>
+    <van-icon name="chat-o" size="40"></van-icon>
+    <van-icon name="chat-o" size="3rem"></van-icon>
   </div>
 </template>
 
