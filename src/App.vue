d0585f111b456c88ff74ac69b4aa3ff7d01cb568
jinyanwei <jinyanwei@live.com>
2020-07-18 23:47:18 +0800

    cell 分组标题

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <div id="app">
     <div style="height: 10px"></div>
-    <van-cell-group>
-      <van-cell title="URL 跳转" is-link url="/vant/mobile.html"></van-cell>
-      <van-cell title="路由跳转" is-link to="index"></van-cell>
+    <van-cell-group title="分组1">
+      <van-cell title="单元格" value="内容"></van-cell>
+    </van-cell-group>
+    <van-cell-group title="分组2">
+      <van-cell title="单元格" value="内容"></van-cell>
     </van-cell-group>
   </div>
 </template>
