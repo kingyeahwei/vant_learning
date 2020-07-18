079d2d41bfebb5736d4154e79080571fb3e8578e
jinyanwei <jinyanwei@live.com>
2020-07-19 00:17:09 +0800

    image 圆形图片

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <van-image height="10rem" width="10rem" fit="contain" src="https://img.yzcdn.cn/vant/cat.jpeg"></van-image>
+    <van-image height="10rem" width="10rem" round src="https://img.yzcdn.cn/vant/cat.jpeg"></van-image>
   </div>
 </template>
 
