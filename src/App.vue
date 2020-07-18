5927c105f63c3eb049c19c71bc69fabc56a8b540
jinyanwei <jinyanwei@live.com>
2020-07-18 19:45:38 +0800

    button 朴素按钮

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,7 @@
 <template>
   <div id="app">
-    <van-button type="default">默认按钮</van-button>
-    <van-button type="primary">主要按钮</van-button>
-    <van-button type="info">信息按钮</van-button>
-    <van-button type="warning">警告按钮</van-button>
-    <van-button type="danger">危险按钮</van-button>
+    <van-button plain type="primary">朴素按钮</van-button>
+    <van-button plain type="info">朴素按钮</van-button>
   </div>
 </template>
 
