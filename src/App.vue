25a49d6c8ce4a68c5ab961666501b3a424e7a677
jinyanwei <jinyanwei@live.com>
2020-07-18 23:29:28 +0800

    button 页面导航

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
-    <van-button type="primary" block>块级元素</van-button>
+    <van-button type="primary" url="/vant/mobile.html">URL跳转</van-button>
+    <van-button type="primary" to="index">路由跳转</van-button>
   </div>
 </template>
 
