78b309f637d6c6731556514552d7e8f07efbc2de
jinyanwei <jinyanwei@live.com>
2020-07-18 23:39:27 +0800

    cell 展示图标

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,7 @@
   <div id="app">
     <div style="height: 10px"></div>
     <van-cell-group>
-      <van-cell title="单元格" value="内容" size="large"></van-cell>
-      <van-cell title="单元格" value="内容" size="large" label="描述信息"></van-cell>
+      <van-cell title="单元格" icon="location-o"></van-cell>
     </van-cell-group>
   </div>
 </template>
