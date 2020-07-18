f3317f8c28ed374346c276c947973ae7cb5389a1
jinyanwei <jinyanwei@live.com>
2020-07-18 23:38:28 +0800

    cell 单元格大小

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,16 @@
   <div id="app">
     <div style="height: 10px"></div>
     <van-cell-group>
-      <van-cell title="单元格" value="内容"></van-cell>
-      <van-cell title="单元格" value="内容" label="描述信息"></van-cell>
+      <van-cell title="单元格" value="内容" size="large"></van-cell>
+      <van-cell title="单元格" value="内容" size="large" label="描述信息"></van-cell>
     </van-cell-group>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+  export default {
+    name: 'App'
+  }
 </script>
 
 <style>
