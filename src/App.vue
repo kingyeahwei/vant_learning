bc392e7355c2dca8f23af47e0fbc44ac9cd09041
jinyanwei <jinyanwei@live.com>
2020-07-18 23:40:27 +0800

    cell 只设置value

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div style="height: 10px"></div>
     <van-cell-group>
-      <van-cell title="单元格" icon="location-o"></van-cell>
+      <van-cell value="内容"></van-cell>
     </van-cell-group>
   </div>
 </template>
