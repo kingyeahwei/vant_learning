5f5adc5b56484b5937f6e8b01dcae45d46e3186e
jinyanwei <jinyanwei@live.com>
2020-07-18 23:52:24 +0800

    cell 使用插槽

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <template>
   <div id="app">
     <div style="height: 10px"></div>
-    <van-cell-group title="分组1">
-      <van-cell title="单元格" value="内容"></van-cell>
-    </van-cell-group>
-    <van-cell-group title="分组2">
-      <van-cell title="单元格" value="内容"></van-cell>
-    </van-cell-group>
+    <van-cell value="内容" is-link>
+      <template #title>
+        <span class="custom-title">单元格</span>
+        <van-tag type="danger">标签</van-tag>
+      </template>
+    </van-cell>
+    <van-cell title="单元格" icon="shop-o">
+      <template #right-icon>
+        <van-icon name="search" style="font-size: 16px; line-height: inherit;"></van-icon>
+      </template>
+    </van-cell>
   </div>
 </template>
 
