7617d6ec709a541e414af84594f2769595643f7a
jinyanwei <jinyanwei@live.com>
2020-07-18 23:32:12 +0800

    button 自定义颜色

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="app">
-    <van-button type="primary" url="/vant/mobile.html">URL跳转</van-button>
-    <van-button type="primary" to="index">路由跳转</van-button>
+    <van-button color="#7232dd">单色按钮</van-button>
+    <van-button color="#7232dd" plain>单色按钮</van-button>
+    <van-button color="linear-gradient(to right, #4bb0ff, #6149f6)">渐变色按钮</van-button>
   </div>
 </template>
 
