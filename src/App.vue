cf336f7eabe8b4662b39f061976e270f1d1b797c
jinyanwei <jinyanwei@live.com>
2020-07-18 23:43:08 +0800

    cell 展示箭头

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,9 @@
   <div id="app">
     <div style="height: 10px"></div>
     <van-cell-group>
-      <van-cell value="内容"></van-cell>
+      <van-cell title="单元格" is-link></van-cell>
+      <van-cell title="单元格" is-link value="内容"></van-cell>
+      <van-cell title="单元格" is-link arrow-direction="down" value="内容"></van-cell>
     </van-cell-group>
   </div>
 </template>
