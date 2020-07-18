2094afe8e7c583e1133c9e7b98bff112c4920474
jinyanwei <jinyanwei@live.com>
2020-07-18 23:19:22 +0800

    button 加载状态

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="app">
-    <van-button disabled type="primary">禁用状态</van-button>
-    <van-button disabled type="info">禁用状态</van-button>
+    <van-button loading type="primary"></van-button>
+    <van-button loading type="primary" loading-type="spinner"></van-button>
+    <van-button loading type="info" loading-text="加载中..."></van-button>
   </div>
 </template>
 
